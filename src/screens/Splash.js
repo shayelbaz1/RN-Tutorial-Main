55cbdd1ae72a8653fadd51e71b68ff0a73dfea4c
Shay Elbaz <shaysha6@gmail.com>
2021-12-16 19:38:17 +0200

    format

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,63 +1,50 @@
-import React, { useEffect } from 'react';
-import {
-    View,
-    StyleSheet,
-    Text,
-    Image,
-} from 'react-native';
-import PushNotification from "react-native-push-notification";
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
+import PushNotification from 'react-native-push-notification';
 import GlobalStyle from '../utils/GlobalStyle';
 
-export default function Splash({ navigation }) {
+export default function Splash({navigation}) {
+  useEffect(() => {
+    createChannels();
+    setTimeout(() => {
+      navigation.replace('My Tasks');
+    }, 2000);
+  }, []);
 
-    useEffect(() => {
-        createChannels();
-        setTimeout(() => {
-            navigation.replace('My Tasks');
-        }, 2000);
-    }, []);
+  const createChannels = () => {
+    PushNotification.createChannel({
+      channelId: 'task-channel',
+      channelName: 'Task Channel',
+    });
+  };
 
-    const createChannels = () => {
-        PushNotification.createChannel(
-            {
-                channelId: "task-channel",
-                channelName: "Task Channel"
-            }
-        )
-    }
-
-    return (
-        <View style={styles.body} >
-            <Image
-                style={styles.logo}
-                source={require('../../assets/checklist.png')}
-            />
-            <Text
-                style={[
-                    GlobalStyle.CustomFontBig,
-                    styles.text
-                ]}
-            >
-                Mash To-Do List
-            </Text>
-        </View>
-    )
+  return (
+    <View style={styles.body}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/checklist.png')}
+      />
+      <Text style={[GlobalStyle.CustomFontBig, styles.text]}>
+        Mash To-Do List
+      </Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#0080ff',
-    },
-    logo: {
-        width: 150,
-        height: 150,
-        margin: 20,
-    },
-    text: {
-        fontSize: 40,
-        color: '#ffffff',
-    },
-})
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#0080ff',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    margin: 20,
+  },
+  text: {
+    fontSize: 40,
+    color: '#ffffff',
+  },
+});
