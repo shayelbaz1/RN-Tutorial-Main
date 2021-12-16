ca68e8ecfe8cadb6d4baa6f7e6ac1453036e5132
Shay Elbaz <shaysha6@gmail.com>
2021-12-16 20:16:31 +0200

    sectionList

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   View,
@@ -10,63 +10,62 @@ import {
 } from 'react-native';
 
 const App = () => {
-
   const [Items, setItems] = useState([
-    { name: 'Item 1' },
-    { name: 'Item 2' },
-    { name: 'Item 3' },
-    { name: 'Item 4' },
-    { name: 'Item 5' },
-    { name: 'Item 6' },
-    { name: 'Item 7' },
-    { name: 'Item 8' },
-    { name: 'Item 9' },
-    { name: 'Item 27' },
-    { name: 'Item 78' },
+    {name: 'Item 1'},
+    {name: 'Item 2'},
+    {name: 'Item 3'},
+    {name: 'Item 4'},
+    {name: 'Item 5'},
+    {name: 'Item 6'},
+    {name: 'Item 7'},
+    {name: 'Item 8'},
+    {name: 'Item 9'},
+    {name: 'Item 27'},
+    {name: 'Item 78'},
   ]);
   const DATA = [
     {
-      title: 'Title 1',
+      header: 'Header 1',
       data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
     },
     {
-      title: 'Title 2',
+      header: 'Header 2',
       data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
     },
     {
-      title: 'Title 3',
+      header: 'Header 3',
       data: ['Item 3-1'],
     },
     {
-      title: 'Title 4',
+      header: 'Header 4',
       data: ['Item 4-1', 'Item 4-2'],
     },
-  ]
+  ];
   const [Refreshing, setRefreshing] = useState(false);
 
   const onRefresh = () => {
     setRefreshing(true);
-    setItems([...Items, { name: 'Item 69' }]);
+    setItems([...Items, {name: 'Item 69'}]);
     setRefreshing(false);
-  }
+  };
 
   return (
     <SectionList
       keyExtractor={(item, index) => index.toString()}
       sections={DATA}
-      renderItem={({ item }) => (
-        <Text style={styles.text}>{item}</Text>
-      )}
-      renderSectionHeader={({section})=>(
+      renderItem={({item}) => <Text style={styles.text}>{item}</Text>}
+      renderSectionHeader={({section}) => (
         <View style={styles.item}>
-          <Text style={styles.text}>{section.title}</Text>
+          <Text style={styles.text}>{section.header}</Text>
         </View>
       )}
     />
     // <FlatList
+    //   // numColumns={3}
+    //   horizontal
     //   keyExtractor={(item, index) => index.toString()}
     //   data={Items}
-    //   renderItem={({ item }) => (
+    //   renderItem={({item}) => (
     //     <View style={styles.item}>
     //       <Text style={styles.text}>{item.name}</Text>
     //     </View>
@@ -107,11 +106,11 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#ffffff',
   },
   item: {
     margin: 10,
-    backgroundColor: '#4ae1fa',
+    borderRadius: 10,
+    backgroundColor: 'lightgrey',
     justifyContent: 'center',
     alignItems: 'center',
   },
