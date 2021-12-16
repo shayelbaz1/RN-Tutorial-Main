972a40a0832fa7c7c25ee181effcc9ecd8589f4f
Shay Elbaz <shaysha6@gmail.com>
2021-12-16 19:30:50 +0200

    format code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ToDo from './screens/ToDo';
 import Done from './screens/Done';
 import Task from './screens/Task';
 import Splash from './screens/Splash';
 import Map from './screens/Map';
 import Camera from './screens/Camera';
-import { Provider } from 'react-redux';
-import { Store } from './redux/store';
+import {Provider} from 'react-redux';
+import {Store} from './redux/store';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
@@ -18,33 +17,24 @@ const Tab = createBottomTabNavigator();
 function HomeTabs() {
   return (
     <Tab.Navigator
-      screenOptions={
-        ({ route }) => ({
-          tabBarIcon: ({ focused, size, color }) => {
-            let iconName;
-            if (route.name === 'To-Do') {
-              iconName = 'clipboard-list';
-              size = focused ? 25 : 20;
-            } else if (route.name === 'Done') {
-              iconName = 'clipboard-check';
-              size = focused ? 25 : 20;
-            }
-            return (
-              <FontAwesome5
-                name={iconName}
-                size={size}
-                color={color}
-              />
-            );
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, size, color}) => {
+          let iconName;
+          if (route.name === 'To-Do') {
+            iconName = 'clipboard-list';
+            size = focused ? 25 : 20;
+          } else if (route.name === 'Done') {
+            iconName = 'clipboard-check';
+            size = focused ? 25 : 20;
           }
-        })
-      }
+          return <FontAwesome5 name={iconName} size={size} color={color} />;
+        },
+      })}
       tabBarOptions={{
         activeTintColor: '#0080ff',
         inactiveTintColor: '#777777',
-        labelStyle: { fontSize: 15, fontWeight: 'bold' }
-      }}
-    >
+        labelStyle: {fontSize: 15, fontWeight: 'bold'},
+      }}>
       <Tab.Screen name={'To-Do'} component={ToDo} />
       <Tab.Screen name={'Done'} component={Done} />
     </Tab.Navigator>
@@ -62,15 +52,14 @@ function App() {
           screenOptions={{
             headerTitleAlign: 'center',
             headerStyle: {
-              backgroundColor: '#0080ff'
+              backgroundColor: '#0080ff',
             },
             headerTintColor: '#ffffff',
             headerTitleStyle: {
               fontSize: 25,
-              fontWeight: 'bold'
-            }
-          }}
-        >
+              fontWeight: 'bold',
+            },
+          }}>
           <RootStack.Screen
             name="Splash"
             component={Splash}
@@ -78,22 +67,13 @@ function App() {
               headerShown: false,
             }}
           />
-          <RootStack.Screen
-            name="My Tasks"
-            component={HomeTabs}
-          />
-          <RootStack.Screen
-            name="Task"
-            component={Task}
-          />
-          <RootStack.Screen
-            name="Camera"
-            component={Camera}
-          />
+          <RootStack.Screen name="My Tasks" component={HomeTabs} />
+          <RootStack.Screen name="Task" component={Task} />
+          <RootStack.Screen name="Camera" component={Camera} />
         </RootStack.Navigator>
       </NavigationContainer>
     </Provider>
-  )
+  );
 }
 
 export default App;
