6abd156adad76c712ce1b7dd521a51cbcca37732
Shay Elbaz <shaysha6@gmail.com>
2021-12-16 21:26:06 +0200

    pressable button style pressed {...props.style}

--- src/Header.js
+++ src/Header.js
@@ -1,37 +1,29 @@
 import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Header = (props) => {
-    return (
-        <View
-            style={styles.view}
-        >
-            <Text
-                style={styles.text}
-            >
-                React Native Tutorial
-            </Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.view}>
+      <Text style={styles.text}>React Native Tutorial</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    view: {
-        width:'100%',
-        height: 50,
-        backgroundColor: '#00f',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text:{
-        fontSize: 25,
-        fontWeight:'bold',
-        color:'#ffffff'
-    }
-})
+  view: {
+    width: '100%',
+    height: 80,
+    paddingBottom: 10,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+});
 
 export default Header;
