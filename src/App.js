9db3f349fe08f6b32619062b82b74427adec8ab0
Shay Elbaz <shaysha6@gmail.com>
2021-12-17 10:36:37 +0200

    drawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -9,18 +8,18 @@ import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 const Drawer = createDrawerNavigator();
 
 function App() {
-  return (
+  return ( 
     <NavigationContainer>
       <Drawer.Navigator
         initialRouteName="Screen_A"
-        drawerPosition='left'
+        drawerPosition="left"
         drawerType="front"
         edgeWidth={100}
         hideStatusBar={false}
-        overlayColor='#00000090'
+        overlayColor="#00000090"
         drawerStyle={{
           backgroundColor: '#e6e6e6',
-          width: 250
+          width: 250,
         }}
         screenOptions={{
           headerShown: true,
@@ -28,27 +27,26 @@ function App() {
           gestureEnabled: true,
           headerTitleAlign: 'center',
           headerStyle: {
-            backgroundColor: '#0080ff'
+            backgroundColor: '#0080ff',
           },
-          headerTintColor: '#ffffff',
+          headerTintColor: '#000',
           headerTitleStyle: {
             fontSize: 25,
-            fontWeight: 'bold'
-          }
-        }}
-      >
+            fontWeight: 'bold',
+          },
+        }}>
         <Drawer.Screen
           name="Screen_A"
           component={ScreenA}
           options={{
             title: 'Screen_A Title',
-            drawerIcon: ({ focused }) => (
+            drawerIcon: ({focused}) => (
               <FontAwesome5
                 name="autoprefixer"
                 size={focused ? 25 : 20}
                 color={focused ? '#0080ff' : '#999999'}
               />
-            )
+            ),
           }}
         />
         <Drawer.Screen
@@ -56,18 +54,18 @@ function App() {
           component={ScreenB}
           options={{
             title: 'Screen_B Title',
-            drawerIcon: ({ focused }) => (
+            drawerIcon: ({focused}) => (
               <FontAwesome5
                 name="btc"
                 size={focused ? 25 : 20}
                 color={focused ? '#0080ff' : '#999999'}
               />
-            )
+            ),
           }}
         />
       </Drawer.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default App;
