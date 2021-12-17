b78e512d44e57c09c5a154d2d68e394d2946854c
Shay Elbaz <shaysha6@gmail.com>
2021-12-17 13:00:12 +0200

    font

--- src/screens/ScreenA.js
+++ src/screens/ScreenA.js
@@ -58,13 +58,14 @@ export default function ScreenA({ navigation, route }) {
 }
 
 const styles = StyleSheet.create({
-    body: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 40,
-        margin: 10,
-    }
-})
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 40,
+    margin: 10,
+    fontFamily: 'DancingScript-Regular'
+  },
+});
