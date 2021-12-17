6a47d9f054f533334b9c2341f3cfa6d816ae1ad9
Shay Elbaz <shaysha6@gmail.com>
2021-12-17 02:25:06 +0200

    stack to tab vector icons tabBarOptions Icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -16,8 +15,8 @@ function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, size, color }) => {
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, size, color}) => {
             let iconName;
             if (route.name === 'Screen_A') {
               iconName = 'autoprefixer';
@@ -28,40 +27,30 @@ function App() {
               size = focused ? 25 : 20;
               // color = focused ? '#f0f' : '#555';
             }
-            return (
-              <FontAwesome5
-                name={iconName}
-                size={size}
-                color={color}
-              />
-            )
-          }
+            return <FontAwesome5 name={iconName} size={size} color={color} />;
+          },
         })}
         tabBarOptions={{
           activeTintColor: '#f0f',
           inactiveTintColor: '#555',
           activeBackgroundColor: '#fff',
           inactiveBackgroundColor: '#999',
-          showLabel: true,
-          labelStyle: { fontSize: 14 },
+          showLabel: false,
+          labelStyle: {fontSize: 14},
           showIcon: true,
         }}
-        activeColor='#f0edf6'
-        inactiveColor='#3e2465'
-        barStyle={{ backgroundColor: '#694fad' }}
-      >
+        activeColor="#f0edf6"
+        inactiveColor="#3e2465"
+        barStyle={{backgroundColor: '#694fad'}}>
         <Tab.Screen
           name="Screen_A"
           component={ScreenA}
-          options={{ tabBarBadge: 3 }}
+          options={{tabBarBadge: 3}}
         />
-        <Tab.Screen
-          name="Screen_B"
-          component={ScreenB}
-        />
+        <Tab.Screen name="Screen_B" component={ScreenB} />
       </Tab.Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default App;
